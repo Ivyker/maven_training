983f422d18bbf4865aa83cedc56d96aac4fbfd34
Ivyker <rmili-riou@et.esiea.fr>
2022-10-06 01:45:42 +0200

    Add test to match 100% coverage

--- src/main/java/fr/lernejo/SampleTest.java
+++ src/main/java/fr/lernejo/SampleTest.java
@@ -1,4 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
-class SampleTest {
-  
+package fr.lernejo;
+
+import org.assertj.core.api.Assertions;
+
+class Test {
+
+    @org.junit.jupiter.api.Test
+    void sum_of_10_and_5_equal_15(){
+        int u = 10;
+        int v = 5;
+
+        int sum = Sample.op(Sample.Operation.ADD, u, v);
+
+        Assertions.assertThat(sum).as("sum of 10 + 5").isEqualTo(15);
+    }
+    @org.junit.jupiter.api.Test
+    void sum_of_negative_11_and_12_equals_1(){
+        int u = -11;
+        int v = 12;
+
+        int sum = Sample.op(Sample.Operation.ADD, u, v);
+        Assertions.assertThat(sum).as("sum of  -11+12").isEqualTo(1);
+    }
+
+
 }
